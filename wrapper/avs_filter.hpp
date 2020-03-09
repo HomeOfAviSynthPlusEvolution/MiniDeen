aeb7275bb284ac98944eae87bee143610bfa2f3b
Xinyue Lu <i@7086.in>
2020-03-09 02:53:59 -0400

    Fix incorrect pixel type check

--- wrapper/avs_filter.hpp
+++ wrapper/avs_filter.hpp
@@ -60,16 +60,12 @@ public:
   int supported_pixel() const {
     return vi.pixel_type == VideoInfo::CS_I420 || (
       (
-        (vi.pixel_type & VideoInfo::CS_GENERIC_YUV420) == VideoInfo::CS_GENERIC_YUV420
-        || (vi.pixel_type & VideoInfo::CS_GENERIC_YUV422) == VideoInfo::CS_GENERIC_YUV422
-        || (vi.pixel_type & VideoInfo::CS_GENERIC_YUV444) == VideoInfo::CS_GENERIC_YUV444
-        || (vi.pixel_type & VideoInfo::CS_GENERIC_RGBP) == VideoInfo::CS_GENERIC_RGBP
+        (vi.pixel_type & ~VideoInfo::CS_Sample_Bits_Mask) == VideoInfo::CS_GENERIC_YUV420
+        || (vi.pixel_type & ~VideoInfo::CS_Sample_Bits_Mask) == VideoInfo::CS_GENERIC_YUV422
+        || (vi.pixel_type & ~VideoInfo::CS_Sample_Bits_Mask) == VideoInfo::CS_GENERIC_YUV444
+        || (vi.pixel_type & ~VideoInfo::CS_Sample_Bits_Mask) == VideoInfo::CS_GENERIC_RGBP
       ) && (
-        (vi.pixel_type & VideoInfo::CS_Sample_Bits_8) == VideoInfo::CS_Sample_Bits_8
-        || (vi.pixel_type & VideoInfo::CS_Sample_Bits_10) == VideoInfo::CS_Sample_Bits_10
-        || (vi.pixel_type & VideoInfo::CS_Sample_Bits_12) == VideoInfo::CS_Sample_Bits_12
-        || (vi.pixel_type & VideoInfo::CS_Sample_Bits_14) == VideoInfo::CS_Sample_Bits_14
-        || (vi.pixel_type & VideoInfo::CS_Sample_Bits_16) == VideoInfo::CS_Sample_Bits_16
+        (vi.pixel_type & VideoInfo::CS_Sample_Bits_Mask) != VideoInfo::CS_Sample_Bits_32
       )
     );
   }
